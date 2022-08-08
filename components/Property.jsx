3ba0eed367f6d028c4cf48ce4ec6517512303dff
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-08-08 16:42:59 +0530

    linking again

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,45 +1,80 @@
+import { Box, Text, Badge } from '@chakra-ui/layout';
+import { StarIcon } from '@chakra-ui/icons';
+import { Image } from '@chakra-ui/react';
 import Link from 'next/link';
-import Image from 'next/image';
-import { Box, Flex, Text } from '@chakra-ui/layout';
-import { Avatar } from '@chakra-ui/avatar';
-import { FaBed, FaBath } from 'react-icons/fa';
-import { BsGridFill } from 'react-icons/bs';
-import { GoVerified } from 'react-icons/go';
-import millify from 'millify';
 
-const DefaultImage = 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2018_30/1355945/home-exterior-today-180726-tease.jpg';
-
-const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId } }) =>
-(
+const Property = ({ renting, property: { coverPhoto, rooms, title, baths, score_l1, price, randBoostScore, externalId } }) => (
     <Link href={`/property/${externalId}`} passHref>
-        <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer' >
-            <Box>
-                <Image
-                    src={coverPhoto ? coverPhoto.url : DefaultImage}
-                    width={400} height={260}
-                    alt="image"
-                />
-            </Box>
-            <Box w='full'>
-                <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
-                    <Flex alignItems='center'>
-                        <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
-                        <Text fontWeight='bold' fontSize='lg'>AED {price}{rentFrequency && `/${rentFrequency}`}</Text>
-                    </Flex>
-                    <Box>
-                        <Avatar size='sm' src={agency?.logo?.url}></Avatar>
+        <Box
+            bg="white"
+            _dark={{ bg: "gray.800" }}
+            maxW="sm"
+            borderWidth="1px"
+            padding={23}
+            shadow="lg"
+            style={{ cursor: 'pointer' }}
+            _hover={{ transform: 'scale(1.02)' }}
+        >
+            <Image
+                src={coverPhoto.url}
+                alt='property'
+                roundedTop="lg"
+            />
+
+            <Box p="6">
+                <Box display="flex" alignItems="baseline">
+                    <Badge rounded="full" px="2" colorScheme="teal">
+                        New
+                    </Badge>
+                    <Box
+                        color="gray.500"
+                        fontWeight="semibold"
+                        letterSpacing="wide"
+                        fontSize="xs"
+                        textTransform="uppercase"
+                        ml="2"
+                    >
+                        {rooms} Rooms &bull; {baths} baths
                     </Box>
-                </Flex>
-                <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='red.400'>
-                    {rooms}
-                    <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
-                </Flex>
-                <Text fontSize='lg'>
-                    {title.length > 30 ? title.substring(0, 30) + '...' : title}
+                </Box>
+
+                <Text
+                    mt="1"
+                    fontWeight="semibold"
+                    as="h4"
+                    lineHeight="tight"
+                    noOfLines={1}
+                >
+                    {title}
                 </Text>
+
+                <Box>
+                    $ {(price / 10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+
+                    {renting &&
+                        <Box as="span" color="gray.600" fontSize="sm">
+                            / wk
+                        </Box>
+                    }
+
+                </Box>
+
+                <Box display="flex" mt="2" alignItems="center">
+                    {Array(5)
+                        .fill("")
+                        .map((_, i) => (
+                            < StarIcon
+                                key={i}
+                                color={i < (randBoostScore / 100 - 3) ? "teal.500" : "gray.300"}
+                            />
+                        ))}
+                    <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                        {score_l1} reviews
+                    </Box>
+                </Box>
             </Box>
-        </Flex>
+        </Box>
     </Link>
 );
 
-export default Property;
+export default Property;;
--- pages/index.js
+++ pages/index.js
@@ -38,8 +38,11 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       linkName='/search?purpose=for-rent'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
     />
-    <Flex flexWrap='wrap' justifyContent='center'>
-      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+
+    {console.log(propertiesForRent)}
+
+    <Flex flexWrap='wrap' justifyContent='center' gap={5}>
+      {propertiesForRent.map((property) => <Property property={property} key={property.id} renting={true} />)}
     </Flex>
     <Banner
       purpose='BUY A HOME'
@@ -51,7 +54,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       linkName='/search?purpose=for-sale'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
     />
-    <Flex flexWrap='wrap' justifyContent='center'>
+    <Flex flexWrap='wrap' justifyContent='center' gap={5}>
       {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
     </Flex>
   </Box>
