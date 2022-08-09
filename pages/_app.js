193eec98f55a98b71ea289860591ce60f406105a
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-08-09 15:57:16 +0530

    final touches

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,19 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout.jsx';
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false });
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  });
+
   return (
     <>
       <Head>
-
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
       </Head>
 
       <ChakraProvider>
@@ -17,7 +26,7 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
-      
+
     </>
   );
 }
